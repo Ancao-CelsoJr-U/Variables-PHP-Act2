0b11308c50460f21514dc6a779e23a77b81c27c8
Tong <celsoancao5@gmail.com>
2025-08-13 01:16:15 +0000

    First

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Ancao Celso Jr. U.";
+     $age = 18;
+     $school = "Global Reciprocal Colleges";
+     $course = "BSIT";
+     $subject = "WEBSYS";
+     $student = true;
+
+     echo "Name: $name \n";
+     echo "Age: $age \n";
+     echo "School: $school\n";
+     echo "Subject: $subject\n";
+     echo "Student:". (Student ? "Yes" : "No"). "\n";
